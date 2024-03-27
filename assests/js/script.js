e90eb05d0ac9fc94b0039fa16cc7cfd0d5ec73ab
cleciocarmais <franciscoclecioti@gmail.com>
2024-03-26 21:52:49 -0300

    atualizando projeto

--- assests/js/script.js
+++ assests/js/script.js
@@ -18,7 +18,11 @@ const rua = document.getElementById('Rua/Avenida')
 const numeroCasa = document.getElementById('Numero')
 const cartModalFinal = document.getElementById('modal-cart-final')
 const CartEnde = document.querySelectorAll('#Endereço input')
+const InputTroco = document.getElementById("Troco")
+const cartModalCep = document.getElementById('card-ce-invalido')
+const cartPagamento = document.getElementById('cart-forma-pag')
 
+const btncartModelCloseCEP = document.getElementById('close-modalCep-btn2')
 
 const completo = document.getElementById('Completo')
 const fade = document.getElementById('fade')
@@ -26,13 +30,23 @@ console.log(fade);
 
 let card = []
 let total_itens = 0
+window.onload = () => {
+    load()
+};
 
-setTimeout(load, 1280)
 cartBtn.addEventListener('click', () => {
  cartModal.style.display = "flex"
  atualizaCarrinho()
 })
+cartPagamento.addEventListener('click', (evento) => {
+    if (evento.target.value == 'Dinheiro') {
+        InputTroco.style.display = 'flex'
+        
+    } else{
+        InputTroco.style.display = 'none'
 
+    }
+})
 cartModal.addEventListener('click', (evento) =>{
     if(evento.target == cartModal){
         cartModal.style.display = 'none'
@@ -49,6 +63,7 @@ closeModal2.addEventListener('click', () => {
 
 
 })
+// evento caso radio dinheiro for clicado 
 // Clicando no item 
 menu.addEventListener('click', (evento) =>{
     let parentButtom = evento.target.closest(".add-to-cart-btn")
@@ -71,6 +86,7 @@ cartItemsContainer.addEventListener('click', (evento) =>{
 // Colocando cep 
 addressInput.addEventListener("keyup", (ev) =>{
     let inpuValue = ev.target.value
+    inpuValue.replace("-","")
     /// Se tem alguma coisa dentro 
     if (inpuValue) {
         addressWarn.classList.remove("border-red-500")
@@ -94,30 +110,12 @@ checkoutBtn.addEventListener("click", () => {
     fade.style.display = 'flex'
     setTimeout(function(){cartModalFinal.style.display = 'flex'},2000)
     setTimeout(function(){fade.style.display = 'none'},2500)
-    
-    // const cartItemns = card.map(item =>{
-    //     return (`Produto : (${item.name}) \n Quatidade (${item.qta}) \n Preco (${item.preco}) \n`
-    //     )
-
-    // }).join("")
-  
-    // const message = encodeURIComponent(cartItemns)
-    // const phone = 5585991637516
-   
-    //     load2()
-    //     setTimeout(() => {window.open(`https://wa.me/${phone}?text=${message} Endereço : ${addressInput.value}`,"_blank")}, 1284)
-    
-
-    
-    // card.length = 0
-    // addressInput.value = ""
-    // atualizaCarrinho()
-
 })
 checkoutBtn2.addEventListener("click", () => {
+    let pagamendo
    for (let index = 0; index < selectedOption.length; index++) {
     if (selectedOption[index].checked) {
-        console.log(selectedOption[index].value);
+       pagamendo = selectedOption[index].value;
         
     }
     
@@ -138,7 +136,6 @@ checkoutBtn2.addEventListener("click", () => {
     const erros = []
     for (let index = 0; index < CartEnde.length; index++) {
         const element = CartEnde[index];
-        console.log(element);
         if (element.value == "") {
             element.classList.add('border-red-500')
             erros.push(element)
@@ -150,26 +147,39 @@ checkoutBtn2.addEventListener("click", () => {
         return;
         
     }
-    // const cartItemns = card.map(item =>{
-    //     return (`Produto : (${item.name}) \n Quatidade (${item.qta}) \n Preco (${item.preco}) \n`
-    //     )
+    const cartItemns = card.map(item =>{
+        return (`Produto : ${item.name} \n Quatidade  : ${item.qta}  \n Preço : R$ ${item.preco} \n`
+        )
 
-    // }).join("")
+    }).join("")
   
-    // const message = encodeURIComponent(cartItemns)
-    // const phone = 5585991637516
-   
-    //     load2()
-    //     setTimeout(() => {window.open(`https://wa.me/${phone}?text=${message} Endereço : ${addressInput.value}`,"_blank")}, 1284)
+    const message = encodeURIComponent(cartItemns)
+    const phone = 5585991637516
+    const body = `
+    %0A*DETALHES*
+    %0A*Forma de Pagamento* : ${pagamendo}%0A
+    *Endereço*%0A
+    *Cep* : ${addressInput.value}%0A 
+    *Municipio* : ${municipio.value}%0A 
+    *Bairro* : ${bairro.value}%0A 
+    *Endereço* : ${rua.value}%0A 
+    *Numero* : ${numeroCasa.value}%0A 
+    *Ponto de Referencia* : ${completo.value}%0A
     
-
+    `
+        load2()
+        setTimeout(() => {window.open(`https://wa.me/${phone}?text=${message}${body}`,"_blank")}, 1284)
+ 
     
     card.length = 0
     addressInput.value = ""
     atualizaCarrinho()
 
 })
+btncartModelCloseCEP.addEventListener('click', ()=>{
+    cartModalCep.style.display = 'none'
 
+})
 
 function addCarrinho(name, preco){
     // PESQUISA DIFERENÇA ENTRE MAP E FIND
@@ -204,11 +214,11 @@ function atualizaCarrinho(){
     cardItemElement.innerHTML = `
     <div class="flex items-center justify-between">
         <div>
-        <p clas="font-medium">${itens.name}</p>
-        <p>${itens.qta}</p>
-        <p>${itens.preco}</p>
+        <p clas="font-medium"><b class="font-bold">Produto</b> : ${itens.name}</p>
+        <p><b class="font-bold">Quantidade</b> : ${itens.qta}</p>
+        <p><b class="font-bold">R$</b> : ${itens.preco}</p>
         </div>
-        <div><buttom class="remove" data-name="${itens.name}">Remover</buttom></div>
+        <div><buttom class="remove bg-red-500 hover:cursor-pointer text-white px-4   py-1 " data-name="${itens.name}">Remover</buttom></div>
     </di>
     
     `
@@ -257,27 +267,22 @@ async function get_cep(cep){
     fade.style.display = 'flex'
     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const data = await response.json(); // Aguarda a resolução da promessa
+    console.log(response.status);
     console.log(data);
+    if (data.erro != true) {
     fade.style.display = 'none'
     municipio.value = data.localidade
     bairro.value = data.bairro
     rua.value = data.logradouro
+    } else{
+        fade.style.display = 'none'
+        cartModalCep.style.display = 'flex'
+    }
 
 
 
 }
-// {
-//     "cep": "61605-390",
-//     "logradouro": "Avenida Contorno Oeste",
-//     "complemento": "(Cj Planalto Caucaia)",
-//     "bairro": "Planalto Caucaia",
-//     "localidade": "Caucaia",
-//     "uf": "CE",
-//     "ibge": "2303709",
-//     "gia": "",
-//     "ddd": "85",
-//     "siafi": "1373"
-//   }
+
 
 
 
